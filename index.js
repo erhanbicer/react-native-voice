ea5e533342d9fdf6b4da28cee98382b992559200
Erhan BICER <iletisim@erhanbicer.com>
2019-05-21 12:31:49 +0300

    android new version update

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
+'use strict';
+import React, {
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+} from 'react-native';
 
 const { Voice } = NativeModules;
 
 // NativeEventEmitter is only availabe on React Native platforms, so this conditional is used to avoid import conflicts in the browser/server
-const voiceEmitter = Platform.OS !== 'web' ? new NativeEventEmitter(Voice) : null;
+const voiceEmitter = Platform.OS !== "web" ? new NativeEventEmitter(Voice) : null;
 
 class RCTVoice {
   constructor() {
     this._loaded = false;
     this._listeners = null;
     this._events = {
-      onSpeechStart: this._onSpeechStart.bind(this),
-      onSpeechRecognized: this._onSpeechRecognized.bind(this),
-      onSpeechEnd: this._onSpeechEnd.bind(this),
-      onSpeechError: this._onSpeechError.bind(this),
-      onSpeechResults: this._onSpeechResults.bind(this),
-      onSpeechPartialResults: this._onSpeechPartialResults.bind(this),
-      onSpeechVolumeChanged: this._onSpeechVolumeChanged.bind(this),
+      'onSpeechStart': this._onSpeechStart.bind(this),
+      'onSpeechRecognized': this._onSpeechRecognized.bind(this),
+      'onSpeechEnd': this._onSpeechEnd.bind(this),
+      'onSpeechError': this._onSpeechError.bind(this),
+      'onSpeechResults': this._onSpeechResults.bind(this),
+      'onSpeechPartialResults': this._onSpeechPartialResults.bind(this),
+      'onSpeechVolumeChanged': this._onSpeechVolumeChanged.bind(this)
     };
   }
   removeAllListeners() {
@@ -33,12 +38,12 @@ class RCTVoice {
       return Promise.resolve();
     }
     return new Promise((resolve, reject) => {
-      Voice.destroySpeech(error => {
+      Voice.destroySpeech((error) => {
         if (error) {
           reject(new Error(error));
         } else {
           if (this._listeners) {
-            this._listeners.map(listener => listener.remove());
+            this._listeners.map((listener, index) => listener.remove());
             this._listeners = null;
           }
           resolve();
@@ -48,13 +53,12 @@ class RCTVoice {
   }
   start(locale, options = {}) {
     if (!this._loaded && !this._listeners && voiceEmitter !== null) {
-      this._listeners = Object.keys(this._events).map(key =>
-        voiceEmitter.addListener(key, this._events[key]),
-      );
+      this._listeners = Object.keys(this._events)
+        .map((key, index) => voiceEmitter.addListener(key, this._events[key]));
     }
 
     return new Promise((resolve, reject) => {
-      const callback = error => {
+      const callback = (error) => {
         if (error) {
           reject(new Error(error));
         } else {
@@ -62,19 +66,12 @@ class RCTVoice {
         }
       };
       if (Platform.OS === 'android') {
-        Voice.startSpeech(
-          locale,
-          Object.assign(
-            {
-              EXTRA_LANGUAGE_MODEL: 'LANGUAGE_MODEL_FREE_FORM',
-              EXTRA_MAX_RESULTS: 5,
-              EXTRA_PARTIAL_RESULTS: true,
-              REQUEST_PERMISSIONS_AUTO: true,
-            },
-            options,
-          ),
-          callback,
-        );
+        Voice.startSpeech(locale, Object.assign({
+          EXTRA_LANGUAGE_MODEL: "LANGUAGE_MODEL_FREE_FORM",
+          EXTRA_MAX_RESULTS: 5,
+          EXTRA_PARTIAL_RESULTS: true,
+          REQUEST_PERMISSIONS_AUTO: true,
+        }, options), callback);
       } else {
         Voice.startSpeech(locale, callback);
       }
@@ -85,7 +82,7 @@ class RCTVoice {
       return Promise.resolve();
     }
     return new Promise((resolve, reject) => {
-      Voice.stopSpeech(error => {
+      Voice.stopSpeech((error) => {
         if (error) {
           reject(new Error(error));
         } else {
@@ -99,7 +96,7 @@ class RCTVoice {
       return Promise.resolve();
     }
     return new Promise((resolve, reject) => {
-      Voice.cancelSpeech(error => {
+      Voice.cancelSpeech((error) => {
         if (error) {
           reject(new Error(error));
         } else {
@@ -119,20 +116,8 @@ class RCTVoice {
       });
     });
   }
-
-  /**
-   * (Android) Get a list of the speech recognition engines available on the device
-   * */
-  getSpeechRecognitionServices() {
-    if (Platform.OS !== 'android') {
-      throw new Exception('Speech recognition services can be queried for only on Android');
-    }
-
-    return Voice.getSpeechRecognitionServices();
-  }
-
   isRecognizing() {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       Voice.isRecognizing(isRecognizing => resolve(isRecognizing));
     });
   }
